da381b7adc327f9397a2801f0230e7623550025c
Stoner5 <Stoner5@github.com>
2020-01-06 15:31:43 +0600

    2nd change

--- app/src/main/java/com/e/mydemo/MainActivity.java
+++ app/src/main/java/com/e/mydemo/MainActivity.java
@@ -1,14 +1,21 @@
 package com.e.mydemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv);
+        textView.setText("My World !!!");
     }
 }
